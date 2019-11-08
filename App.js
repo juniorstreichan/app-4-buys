b75a9981ecc48afe86f049552918ce70b3f5c737
Junior Streichan <juniorstreichan.dev@gmail.com>
2019-11-08 03:51:35 -0400

    #1 react native init

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>TESTE</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
