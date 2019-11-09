55422b22f36b67c99bb934c1feccba316d098601
Junior Streichan <juniorstreichan.dev@gmail.com>
2019-11-09 08:12:11 -0400

    #8 Apk release gerado closes #8

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import {AppRegistry} from 'react-native';
+import {AppRegistry, YellowBox} from 'react-native';
 import App from './src';
 import {name as appName} from './app.json';
 
+YellowBox.ignoreWarnings(['UNSAFE_']);
 AppRegistry.registerComponent(appName, () => App);
